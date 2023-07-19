5fe5d4b40fd9989de4c7e5377ddfaa0f34a73dfd
김윤하 <voyahoo12396@gmail.com>
2023-07-19 11:31:51 +0900

    pomodoros 플레이 일시정지 리셋 기능 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
@@ -10,7 +11,20 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
+        ),
+        cardColor: const Color(0xFFF4EDDB),
+      ),
+      home: const Scaffold(
+        body: HomeScreen(),
+      ),
     );
   }
 }
