4910ee589dcb0622ab47118721c8547993bac423
김윤하 <voyahoo12396@gmail.com>
2023-07-25 09:21:44 +0900

    api데이터를 보여질 화면에 데이터만 가져옴 아직 ui가 보이지는 않음

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,10 +3,11 @@ import 'package:toonflix/models/webtoon_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
 
-  Future<List<WebtoonModel>> getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
@@ -15,7 +16,6 @@ class ApiService {
       for (var webtoon in webtoons) {
         webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
-
       return webtoonInstances;
     }
     throw Error();
